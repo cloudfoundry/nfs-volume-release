41209f9e273813067e7ee96b5cbc1483aa2e8fb0
Julian Hjortshoj <julian.hjortshoj@emc.com>
2017-04-05 13:35:20 -0700

    bump nfsdriver
    Submodule src/code.cloudfoundry.org/nfsdriver de6440c..6958adc:
      > create directory for serialized state in the case where it doesn't already exist [#142358289](https://www.pivotaltracker.com/story/show/142358289)

--- scripts/fly-exec-certs.sh
+++ scripts/fly-exec-certs.sh
@@ -2,5 +2,5 @@
 set -e
 
 scripts_path=./$(dirname $0)
-
+export SOURCE=nfs://10.10.200.72/export2/certs
 fly -t persi execute -c $scripts_path/ci/run_driver_cert_tcp.build.yml -i nfs-volume-release=/Users/pivotal/workspace/nfs-volume-release -i lib-nfs=/Users/pivotal/workspace/libnfs -i fuse-nfs=/Users/pivotal/workspace/fuse-nfs --privileged
