8aede6c24e6e8e178a162244646e90d6d794f7ba
Dave Walter <dwalter@pivotal.io>
2018-10-30 11:24:09 -0700

    Add run_driver_units scripts for windows [#161559803](https://www.pivotaltracker.com/story/show/161559803)
    Co-authored-by: Paul Warren <paul.warren@emc.com>

---
+++ scripts/ci/run_driver_unit_windows.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = “Stop”;
+trap { $host.SetShouldExit(1) }
+
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        if ($CleanupScript) {
+            "Executing cleanup script: $CleanupScript"
+            &$CleanupScript
+        }
+        $msg = @"
+EXE RETURNED EXIT CODE $LastExitCode
+CALLSTACK:$(Get-PSCallStack | Out-String)
+"@
+        throw $msg
+    }
+}
+
+cd nfs-volume-release
+
+$env:GOPATH=$PWD
+$env:PATH="$PWD/bin;$env:PATH"
+
+go install github.com/onsi/ginkgo/ginkgo
+
+cd src/code.cloudfoundry.org/nfsdriver/mountchecker
+ginkgo -v -r -keepGoing -p -trace -randomizeAllSpecs -progress --race
+
+CheckLastExitCode
