ea3de9855532cfaa0a87ff99d47986ddabb2cd9b
Dave Walter <dwalter@pivotal.io>
2018-11-06 08:09:15 -0800

    Update nfsbroker-bbr config.json tests
    - to use same context/it pattern as the nfsbrokerpush start.sh tests
    
    [#161739430](https://www.pivotaltracker.com/story/show/161739430)

--- spec/jobs/nfsbroker-bbr/config_json_spec.rb
+++ spec/jobs/nfsbroker-bbr/config_json_spec.rb
@@ -9,107 +9,135 @@ describe 'nfsbroker-bbr job' do
   describe 'config.json' do
     let(:template) { job.template('config/config.json') }
 
-    it 'generates successfully' do
-      tpl_output = template.render(
-        "nfsbroker" => {
-          "db_hostname" => "some-db-host",
-          "db_username" => "some-db-user",
-          "db_password" => "some-db-password",
-          "db_port" => "some-db-port",
-          "db_name" => "some-db-name",
-          "db_driver" => "some-db-driver",
-        }
-      )
-      config = JSON.parse(tpl_output)
-      expect(config).to eq({
-        "username" => "some-db-user",
-        "password" => "some-db-password",
-        "host" => "some-db-host",
-        "port" => "some-db-port",
-        "database" => "some-db-name",
-        "adapter" => "some-db-driver",
-      })
-    end
-
-    it 'includes the CA cert if specified' do
-      tpl_output = template.render(
-        "nfsbroker" => {
-          "db_hostname" => "some-db-host",
-          "db_username" => "some-db-user",
-          "db_password" => "some-db-password",
-          "db_port" => "some-db-port",
-          "db_name" => "some-db-name",
-          "db_driver" => "some-db-driver",
-          "db_ca_cert" => "some-ca-cert",
-        }
-      )
-      config = JSON.parse(tpl_output)
-      expect(config).to eq({
-        "username" => "some-db-user",
-        "password" => "some-db-password",
-        "host" => "some-db-host",
-        "port" => "some-db-port",
-        "database" => "some-db-name",
-        "adapter" => "some-db-driver",
-        "tls" => {
-          "cert" => {
-            "ca" => "some-ca-cert",
+    context 'when configured with minimal properties' do
+      let(:manifest_properties) do
+        {
+          "nfsbroker" => {
+            "db_hostname" => "some-db-host",
+            "db_username" => "some-db-user",
+            "db_password" => "some-db-password",
+            "db_port" => "some-db-port",
+            "db_name" => "some-db-name",
+            "db_driver" => "some-db-driver",
           }
         }
-      })
+      end
+
+      it 'generates successfully' do
+        tpl_output = template.render(manifest_properties)
+
+        config = JSON.parse(tpl_output)
+        expect(config).to eq({
+          "username" => "some-db-user",
+          "password" => "some-db-password",
+          "host" => "some-db-host",
+          "port" => "some-db-port",
+          "database" => "some-db-name",
+          "adapter" => "some-db-driver",
+        })
+      end
     end
 
-    it 'sets the flag to skip db hostname validation' do
-      tpl_output = template.render(
-        "nfsbroker" => {
-          "db_hostname" => "some-db-host",
-          "db_username" => "some-db-user",
-          "db_password" => "some-db-password",
-          "db_port" => "some-db-port",
-          "db_name" => "some-db-name",
-          "db_driver" => "some-db-driver",
-          "db_ca_cert" => "some-ca-cert",
-          "db_skip_hostname_validation" => true,
+    context 'when a CA cert is specified' do
+      let(:manifest_properties) do
+        {
+          "nfsbroker" => {
+            "db_hostname" => "some-db-host",
+            "db_username" => "some-db-user",
+            "db_password" => "some-db-password",
+            "db_port" => "some-db-port",
+            "db_name" => "some-db-name",
+            "db_driver" => "some-db-driver",
+            "db_ca_cert" => "some-ca-cert",
+          }
         }
-      )
-      config = JSON.parse(tpl_output)
-      expect(config).to eq({
-        "username" => "some-db-user",
-        "password" => "some-db-password",
-        "host" => "some-db-host",
-        "port" => "some-db-port",
-        "database" => "some-db-name",
-        "adapter" => "some-db-driver",
-        "tls" => {
-          "cert" => {
-            "ca" => "some-ca-cert",
-          },
-          "skip_host_verify" => true,
-        }
-      })
+      end
+
+      it 'includes the CA cert' do
+        tpl_output = template.render(manifest_properties)
+
+        config = JSON.parse(tpl_output)
+        expect(config).to eq({
+          "username" => "some-db-user",
+          "password" => "some-db-password",
+          "host" => "some-db-host",
+          "port" => "some-db-port",
+          "database" => "some-db-name",
+          "adapter" => "some-db-driver",
+          "tls" => {
+            "cert" => {
+              "ca" => "some-ca-cert",
+            }
+          }
+        })
+      end
     end
 
-    it 'ignores the flag to skip db hostname validation if the CA cert is missing' do
-      tpl_output = template.render(
-        "nfsbroker" => {
-          "db_hostname" => "some-db-host",
-          "db_username" => "some-db-user",
-          "db_password" => "some-db-password",
-          "db_port" => "some-db-port",
-          "db_name" => "some-db-name",
-          "db_driver" => "some-db-driver",
-          "db_skip_hostname_validation" => true,
+    context 'when db_skip_hostname_validation is true' do
+      let(:manifest_properties) do
+        {
+          "nfsbroker" => {
+            "db_hostname" => "some-db-host",
+            "db_username" => "some-db-user",
+            "db_password" => "some-db-password",
+            "db_port" => "some-db-port",
+            "db_name" => "some-db-name",
+            "db_driver" => "some-db-driver",
+            "db_ca_cert" => "some-ca-cert",
+            "db_skip_hostname_validation" => true,
+          }
         }
-      )
-      config = JSON.parse(tpl_output)
-      expect(config).to eq({
-        "username" => "some-db-user",
-        "password" => "some-db-password",
-        "host" => "some-db-host",
-        "port" => "some-db-port",
-        "database" => "some-db-name",
-        "adapter" => "some-db-driver",
-      })
+      end
+
+      it 'sets the flag to skip db hostname validation' do
+        tpl_output = template.render(manifest_properties)
+
+        config = JSON.parse(tpl_output)
+        expect(config).to eq({
+          "username" => "some-db-user",
+          "password" => "some-db-password",
+          "host" => "some-db-host",
+          "port" => "some-db-port",
+          "database" => "some-db-name",
+          "adapter" => "some-db-driver",
+          "tls" => {
+            "cert" => {
+              "ca" => "some-ca-cert",
+            },
+            "skip_host_verify" => true,
+          }
+        })
+      end
+    end
+
+    context 'if the CA cert is missing' do
+      let(:manifest_properties) do
+        {
+          "nfsbroker" => {
+            "db_hostname" => "some-db-host",
+            "db_username" => "some-db-user",
+            "db_password" => "some-db-password",
+            "db_port" => "some-db-port",
+            "db_name" => "some-db-name",
+            "db_driver" => "some-db-driver",
+            "db_skip_hostname_validation" => true,
+          }
+        }
+      end
+
+      it 'ignores the flag to skip db hostname validation' do
+        tpl_output = template.render(manifest_properties)
+
+        config = JSON.parse(tpl_output)
+        expect(config).to eq({
+          "username" => "some-db-user",
+          "password" => "some-db-password",
+          "host" => "some-db-host",
+          "port" => "some-db-port",
+          "database" => "some-db-name",
+          "adapter" => "some-db-driver",
+        })
+      end
     end
   end
 end
