8401142e76955e4288039a119289f6a53eed5397
Dennis Leon <dleon@pivotal.io>
2019-05-30 15:23:04 -0700

    Allow running bosh-release tests from the workstation and CI
    [#165890516] https://www.pivotaltracker.com/story/show/165890516
    
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>
    Co-authored-by: Julian Hjortshoj <julian.hjortshoj@emc.com>
    Co-authored-by: Paul Warren <paul.warren@emc.com>

---
+++ scripts/run-bosh-release-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+docker run \
+-t \
+-i \
+--privileged \
+-v /Users/pivotal/workspace/nfs-volume-release/:/nfs-volume-release \
+-v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
+--workdir=/ \
+bosh/main-bosh-docker \
+/nfs-volume-release/scripts/run-inner-bosh-release-tests.sh
---
+++ scripts/run-inner-bosh-release-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eux
+
+start-bosh
+
+export DOCKER_TMP_DIR=$(find /tmp/ -name "tmp.*")
+export DOCKER_HOST=$(ps aux | grep dockerd | grep -o '\-\-host tcp.*4243' | awk '{print $2}')
+
+eval "$(cat /tmp/local-bosh/director/env)"
+
+docker \
+--tls \
+--tlscacert=${DOCKER_TMP_DIR}/ca.pem \
+--tlscert=${DOCKER_TMP_DIR}/cert.pem \
+--tlskey=${DOCKER_TMP_DIR}/key.pem run \
+--network=director_network \
+-v $PWD/nfs-volume-release/:/nfs-volume-release \
+-v $PWD/mapfs-release:/mapfs-release \
+-v /tmp:/tmp \
+-w /nfs-volume-release/src/bosh_release \
+-t \
+-i \
+--env BOSH_ENVIRONMENT=10.245.0.3 \
+--env BOSH_CLIENT=${BOSH_CLIENT} \
+--env BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} \
+--env BOSH_CA_CERT=${BOSH_CA_CERT} \
+--env NFS_VOLUME_RELEASE_PATH=/nfs-volume-release \
+--env MAPFS_RELEASE_PATH=/mapfs-release \
+cfpersi/bosh-release-tests \
+ginkgo .
