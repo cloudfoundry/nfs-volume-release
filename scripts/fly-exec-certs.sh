643ec9c499594cf0990407754d7d563f4d4dc804
Dennis Leon <dleon@pivotal.io>
2019-05-31 12:32:34 -0700

    Remove scripts that are no longer used
    Signed-off-by: Paul Warren <paul.warren@emc.com>
    Co-authored-by: Paul Warren <paul.warren@emc.com>

--- scripts/fly-exec-certs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-scripts_path=./$(dirname $0)
-export SOURCE=nfs://10.10.200.72/export2/certs
-fly -t persi execute -c $scripts_path/ci/run_driver_cert_tcp.build.yml -i nfs-volume-release=/Users/pivotal/workspace/nfs-volume-release -i lib-nfs=/Users/pivotal/workspace/libnfs -i fuse-nfs=/Users/pivotal/workspace/fuse-nfs --privileged
--- scripts/fly-exec-pats.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-scripts_path=./$(dirname $0)
-
-fly -t persi execute -c $scripts_path/ci/run_pats_nfsvolume.build.yml -i nfs-volume-release=/Users/pivotal/workspace/nfs-volume-release
