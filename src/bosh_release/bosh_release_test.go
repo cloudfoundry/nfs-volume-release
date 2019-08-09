7e2da148dcc2984717eca43831fe7a8766f2ed23
Dennis Leon <dleon@pivotal.io>
2019-08-09 16:03:17 -0700

    Add logic to upgrade nfs-common in the event the nfs-common package already exists prior to installing it
    [#167633576](https://www.pivotaltracker.com/story/show/167633576)

--- src/bosh_release/bosh_release_test.go
+++ src/bosh_release/bosh_release_test.go
@@ -16,19 +16,44 @@ var _ = Describe("BoshReleaseTest", func() {
 	})
 
 	It("should have a nfsv3driver process running", func() {
-		expectDpkgInstalled("rpcbind")
-		expectDpkgInstalled("keyutils")
-		expectDpkgInstalled("libevent-2.1-6")
-		expectDpkgInstalled("libnfsidmap2")
-		expectDpkgInstalled("libkrb5support0")
-		expectDpkgInstalled("libk5crypto3")
-		expectDpkgInstalled("libkrb5-3")
-		expectDpkgInstalled("libgssapi-krb5-2")
-		expectDpkgInstalled("nfs-common")
+		expectDpkgInstalled("rpcbind", "0.2.3-0.2")
+		expectDpkgInstalled("keyutils", "1.5.9-8ubuntu1")
+		expectDpkgInstalled("libevent-2.1-6", "2.1.8-stable-4build1")
+		expectDpkgInstalled("libnfsidmap2", "0.25-5")
+		expectDpkgInstalled("libkrb5support0", "1.15.1-2")
+		expectDpkgInstalled("libk5crypto3", "1.15.1-2")
+		expectDpkgInstalled("libkrb5-3", "1.15.1-2")
+		expectDpkgInstalled("libgssapi-krb5-2", "1.15.1-2")
+		expectDpkgInstalled("nfs-common", "1:1.3.4-2.1ubuntu4")
 
 		state := findProcessState("nfsv3driver")
 
 		Expect(state).To(Equal("running"))
+	})
+
+	Context("when an existing package is already installed", func() {
+		BeforeEach(func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo dpkg -P nfs-common")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo apt-get install nfs-common")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+		})
+
+		It("should upgrade the nfs-common package to the version specified in the pre-install script", func() {
+			expectDpkgInstalled("nfs-common", "1:1.2.8-9ubuntu12.2")
+
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo /var/vcap/jobs/nfsv3driver/bin/pre-start")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			expectDpkgInstalled("nfs-common", "1:1.3.4-2.1ubuntu4")
+		})
 	})
 
 	Context("when nfsv3driver is disabled", func() {
@@ -114,11 +139,11 @@ func expectDpkgNotInstalled(dpkgName string) {
 	Eventually(session).Should(gexec.Exit(1), string(session.Out.Contents()))
 }
 
-func expectDpkgInstalled(dpkgName string) {
-	packageDebugMessage := fmt.Sprintf("Expecting dpkg %s to be installed", dpkgName)
+func expectDpkgInstalled(dpkgName string, version string) {
+	packageDebugMessage := fmt.Sprintf("Expecting dpkg %s %s to be installed", dpkgName, version)
 	By(packageDebugMessage)
 
-  cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", fmt.Sprintf("dpkg -s %s", dpkgName))
+  cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", fmt.Sprintf("dpkg -s %s | grep Version | grep -o '[0-9].*' | grep -E '^%s$'", dpkgName, version))
   session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
   Expect(err).NotTo(HaveOccurred(), packageDebugMessage)
   Eventually(session).Should(gexec.Exit(0), packageDebugMessage + string(session.Out.Contents()))
