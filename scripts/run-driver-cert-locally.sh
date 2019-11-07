2afa4ac9ecbdfaade93d32ea510325ddb6ebf1ef
Julian Hjortshoj <julian.hjortshoj@emc.com>
2019-11-07 15:06:19 -0800

    fix scripts for invoking tests locally

--- scripts/run-driver-cert-locally.sh
+++ scripts/run-driver-cert-locally.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 docker run -it \
--v /Users/pivotal/workspace/nfs-volume-release/src:/go/src/ \
--v /Users/pivotal/workspace/nfs-volume-release:/nfs-volume-release \
--v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
+-v $HOME/workspace/nfs-volume-release/src:/go/src/ \
+-v $HOME/workspace/docker_driver_integration_tests:/docker_driver_integration_tests \
+-v $HOME/workspace/nfs-volume-release:/nfs-volume-release \
+-v $HOME/workspace/mapfs-release:/mapfs-release \
 -w / \
 -e TRANSPORT=tcp \
+-e TEST_PACKAGE=docker_driver_integration_tests/ \
 --privileged \
 -u root \
 cfpersi/nfs-integration-tests \
