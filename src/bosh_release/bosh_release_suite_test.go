2dceb9ac7797165ac00b1a3c9fb28a3be528b81a
Lisa Cho <lcho@pivotal.io>
2019-11-22 11:33:22 -0800

    Add bosh release tests for 1.7
    [#169635979](https://www.pivotaltracker.com/story/show/169635979)
    
    Co-authored-by: Lisa Cho <lcho@pivotal.io>
    Co-authored-by: Dennis Leon <dleon@pivotal.io>

---
+++ src/bosh_release/bosh_release_suite_test.go
@@ -0,0 +1,143 @@
+package bosh_release_test
+
+import (
+	"encoding/json"
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"os"
+	"os/exec"
+	"time"
+
+	"testing"
+)
+
+func TestBoshReleaseTest(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "BoshReleaseTest Suite")
+}
+
+var dpkgLockBuildPackagePath string
+
+var _ = BeforeSuite(func() {
+	var err error
+
+	dpkgLockBuildPackagePath, err = gexec.BuildIn("/nfs-volume-release", "bosh_release/assets/acquire_dpkg_lock")
+	Expect(err).ShouldNot(HaveOccurred())
+
+	SetDefaultEventuallyTimeout(10 * time.Minute)
+
+	if !hasStemcell() {
+		uploadStemcell()
+	}
+
+	deploy()
+})
+
+func deploy(opsfiles ...string) {
+	deployCmd := []string {"deploy",
+		"-n",
+		"-d",
+		"bosh_release_test",
+		"./nfsv3driver-manifest.yml",
+		"-v", fmt.Sprintf("path_to_nfs_volume_release=%s", os.Getenv("NFS_VOLUME_RELEASE_PATH")),
+		"-v", fmt.Sprintf("path_to_mapfs_release=%s", os.Getenv("MAPFS_RELEASE_PATH")),
+	}
+
+	updatedDeployCmd := make([]string, len(deployCmd))
+	copy(updatedDeployCmd, deployCmd)
+	for _, optFile := range opsfiles {
+		updatedDeployCmd = append(updatedDeployCmd, "-o", optFile)
+	}
+
+	boshDeployCmd := exec.Command("bosh", updatedDeployCmd...)
+	session, err := gexec.Start(boshDeployCmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 60*time.Minute).Should(gexec.Exit(0))
+}
+
+func hasStemcell() bool {
+	boshStemcellsCmd := exec.Command("bosh", "stemcells", "--json")
+	stemcellOutput := gbytes.NewBuffer()
+	session, err := gexec.Start(boshStemcellsCmd, stemcellOutput, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 1*time.Minute).Should(gexec.Exit(0))
+	boshStemcellsOutput := &BoshStemcellsOutput{}
+	Expect(json.Unmarshal(stemcellOutput.Contents(), boshStemcellsOutput)).Should(Succeed())
+	return len(boshStemcellsOutput.Tables[0].Rows) > 0
+}
+
+func uploadStemcell() {
+	boshUsCmd := exec.Command("bosh", "upload-stemcell", "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent")
+	session, err := gexec.Start(boshUsCmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, 20*time.Minute).Should(gexec.Exit(0))
+}
+
+
+func findProcessState(processName string) string {
+
+	boshIsCmd := exec.Command("bosh", "instances", "--ps", "--details", "--json", "--column=process", "--column=process_state")
+
+	boshInstancesOutput := gbytes.NewBuffer()
+	session, err := gexec.Start(boshIsCmd, boshInstancesOutput, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+
+	session = session.Wait(1 * time.Minute)
+	Expect(session.ExitCode()).To(Equal(0), string(boshInstancesOutput.Contents()))
+
+	instancesOutputJson := &BoshInstancesOutput{}
+	err = json.Unmarshal(boshInstancesOutput.Contents(), instancesOutputJson)
+	Expect(err).NotTo(HaveOccurred())
+
+	for _, row := range instancesOutputJson.Tables[0].Rows {
+		if row.Process == processName {
+			return row.ProcessState
+		}
+	}
+
+	return ""
+}
+
+type BoshInstancesOutput struct {
+	Tables []struct {
+		Content string `json:"Content"`
+		Header  struct {
+			Process      string `json:"process"`
+			ProcessState string `json:"process_state"`
+		} `json:"Header"`
+		Rows []struct {
+			Process      string `json:"process"`
+			ProcessState string `json:"process_state"`
+		} `json:"Rows"`
+		Notes interface{} `json:"Notes"`
+	} `json:"Tables"`
+	Blocks interface{} `json:"Blocks"`
+	Lines  []string    `json:"Lines"`
+}
+
+
+type BoshStemcellsOutput struct {
+	Tables []struct {
+		Content string `json:"Content"`
+		Header  struct {
+			Cid     string `json:"cid"`
+			Cpi     string `json:"cpi"`
+			Name    string `json:"name"`
+			Os      string `json:"os"`
+			Version string `json:"version"`
+		} `json:"Header"`
+		Rows []struct {
+			Cid     string `json:"cid"`
+			Cpi     string `json:"cpi"`
+			Name    string `json:"name"`
+			Os      string `json:"os"`
+			Version string `json:"version"`
+		} `json:"Rows"`
+		Notes []string `json:"Notes"`
+	} `json:"Tables"`
+	Blocks interface{} `json:"Blocks"`
+	Lines  []string    `json:"Lines"`
+}
---
+++ src/bosh_release/bosh_release_test.go
@@ -0,0 +1,189 @@
+package bosh_release_test
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"os/exec"
+	"time"
+)
+
+var _ = Describe("BoshReleaseTest", func() {
+	BeforeEach(func() {
+		deploy()
+	})
+
+	It("should have a nfsv3driver process running", func() {
+		expectDpkgInstalled("rpcbind", "0.2.3-0.2")
+		expectDpkgInstalled("keyutils", "1.5.9-8ubuntu1")
+		expectDpkgInstalled("libevent-2.1-6", "2.1.8-stable-4build1")
+		expectDpkgInstalled("libnfsidmap2", "0.25-5")
+		expectDpkgInstalled("libkrb5support0", "1.15.1-2")
+		expectDpkgInstalled("libk5crypto3", "1.15.1-2")
+		expectDpkgInstalled("libkrb5-3", "1.15.1-2")
+		expectDpkgInstalled("libgssapi-krb5-2", "1.15.1-2")
+		expectDpkgInstalled("nfs-common", "1:1.3.4-2.1ubuntu4")
+
+		state := findProcessState("nfsv3driver")
+
+		Expect(state).To(Equal("running"))
+	})
+
+	Context("when an existing package is already installed", func() {
+		BeforeEach(func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo dpkg -P nfs-common")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo apt-get install -y nfs-common")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+		})
+
+		It("should upgrade the nfs-common package to the version specified in the pre-install script", func() {
+			expectDpkgInstalled("nfs-common", "1:1.2.8-9ubuntu12.2")
+
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo /var/vcap/jobs/nfsv3driver/bin/pre-start")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			expectDpkgInstalled("nfs-common", "1:1.3.4-2.1ubuntu4")
+		})
+	})
+
+	Context("when nfsv3driver is disabled", func() {
+		BeforeEach(func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "delete-deployment", "-n")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			deploy("./operations/disable-nfsv3driver.yml")
+		})
+
+
+		It("should not install packages or run rpcbind", func() {
+			expectDpkgNotInstalled("rpcbind")
+			expectDpkgNotInstalled("keyutils")
+			expectDpkgNotInstalled("libevent-2.1-6")
+			expectDpkgNotInstalled("libnfsidmap2")
+			expectDpkgNotInstalled("nfs-common")
+
+			Expect(findProcessState("nfsv3driver")).To(Equal(""))
+		})
+	})
+
+	Context("when another process has a dpkg lock", func() {
+
+		BeforeEach(func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo dpkg -P nfs-common")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo rm -f /tmp/lock_dpkg")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+
+			cmd = exec.Command("bosh", "-d", "bosh_release_test", "scp", dpkgLockBuildPackagePath, "nfsv3driver:/tmp/lock_dpkg")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+
+			cmd = exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo /tmp/lock_dpkg")
+			session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gbytes.Say("locked /var/lib/dpkg/lock"))
+		})
+
+		AfterEach(func() {
+			releaseDpkgLock()
+		})
+
+		It("should successfully dpkg install", func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo /var/vcap/jobs/nfsv3driver/bin/pre-start")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gbytes.Say("dpkg: error: dpkg status database is locked by another process"))
+			releaseDpkgLock()
+			Eventually(session).Should(gexec.Exit(0))
+		})
+
+		It("should eventually timeout when the dpkg lock is not released in a reasonable time", func() {
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo /var/vcap/jobs/nfsv3driver/bin/pre-start")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gbytes.Say("dpkg: error: dpkg status database is locked by another process"))
+			Eventually(session, 6 * time.Minute, 1 * time.Second).Should(gexec.Exit(1))
+		})
+	})
+
+	Context("nfsv3driver drain", func() {
+		It("should successfully drain", func() {
+			By("bosh stopping the nfsv3driver")
+			cmd := exec.Command("bosh", "-d", "bosh_release_test", "stop", "-n", "nfsv3driver")
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+		})
+
+		Context("when nfsv3driver is not reachable", func() {
+			BeforeEach(func() {
+				By("drain cannot reach the nfsv3driver")
+				cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "nfsv3driver", "-c", "sudo iptables -t filter -A OUTPUT -p tcp --dport 7590  -j DROP")
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+			})
+
+			AfterEach(func() {
+				cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "nfsv3driver", "-c", "sudo iptables -t filter -D OUTPUT -p tcp --dport 7590  -j DROP")
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+
+				cmd = exec.Command("bosh", "-d", "bosh_release_test", "start", "-n", "nfsv3driver")
+				session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+			})
+
+			It("should successfully drain", func() {
+				cmd := exec.Command("bosh", "-d", "bosh_release_test", "stop", "-n", "nfsv3driver")
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0), string(session.Out.Contents()))
+			})
+		})
+	})
+})
+
+func releaseDpkgLock() {
+	cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", "sudo pkill lock_dpkg")
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit())
+}
+
+func expectDpkgNotInstalled(dpkgName string) {
+	cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", fmt.Sprintf( "dpkg -l | grep ' %s '", dpkgName))
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit(1), string(session.Out.Contents()))
+}
+
+func expectDpkgInstalled(dpkgName string, version string) {
+	packageDebugMessage := fmt.Sprintf("Expecting dpkg %s %s to be installed", dpkgName, version)
+	By(packageDebugMessage)
+
+  cmd := exec.Command("bosh", "-d", "bosh_release_test", "ssh", "-c", fmt.Sprintf("dpkg -s %s | grep Version | grep -o '[0-9].*' | grep -E '^%s$'", dpkgName, version))
+  session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+  Expect(err).NotTo(HaveOccurred(), packageDebugMessage)
+  Eventually(session).Should(gexec.Exit(0), packageDebugMessage + string(session.Out.Contents()))
+}
