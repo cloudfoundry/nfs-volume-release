430d33bf4b899ed0d380e64cf636ed5cf6cb491d
Julian Hjortshoj <julian.hjortshoj@emc.com>
2019-11-27 10:23:53 -0800

    remove ci scripts that have been relocated to submodules [#169684821](https://www.pivotaltracker.com/story/show/169684821)

--- scripts/fly-execute-nfs-broker-integration-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-fly -t persi \
-execute \
--c ~/workspace/nfs-volume-release/scripts/ci/run_broker_integration.build.yml \
--i nfs-volume-release=$HOME/workspace/nfs-volume-release \
--j persi/nfsbroker-tests \
--p
