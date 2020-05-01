086d7bc49736abd46f64ccd21cd7c3bc88e35a24
Julian Hjortshoj <jhjortshoj@pivotal.io>
2020-05-01 10:29:25 -0700

    update test scripts to use newer parameters and to still work when the home directory is not Users/pivotal [#172568984](https://www.pivotaltracker.com/story/show/172568984)

--- scripts/run-bosh-release-tests.sh
+++ scripts/run-bosh-release-tests.sh
@@ -12,9 +12,10 @@ docker run \
 -t \
 -i \
 --privileged \
--e DEV=true \
--v /Users/pivotal/workspace/nfs-volume-release/:/nfs-volume-release \
--v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
+-v ${HOME}/workspace/nfs-volume-release/:/nfs-volume-release \
+-v ${HOME}/workspace/mapfs-release:/mapfs-release \
 --workdir=/ \
 bosh/main-bosh-docker \
 /nfs-volume-release/scripts/run-bosh-release-tests-in-docker-env.sh
+
+# -e DEV=true \
