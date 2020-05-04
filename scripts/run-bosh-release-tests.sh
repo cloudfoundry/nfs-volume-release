9793744254f6ff6019b1f0a182a86ae87d179c99
Julian Hjortshoj <jhjortshoj@pivotal.io>
2020-05-04 16:38:18 -0700

    Revert "update test scripts to use newer parameters and to still work when the home directory is not Users/pivotal [#172568984](https://www.pivotaltracker.com/story/show/172568984)"
    This reverts commit 086d7bc49736abd46f64ccd21cd7c3bc88e35a24.

--- scripts/run-bosh-release-tests.sh
+++ scripts/run-bosh-release-tests.sh
@@ -12,10 +12,9 @@ docker run \
 -t \
 -i \
 --privileged \
--v ${HOME}/workspace/nfs-volume-release/:/nfs-volume-release \
--v ${HOME}/workspace/mapfs-release:/mapfs-release \
+-e DEV=true \
+-v /Users/pivotal/workspace/nfs-volume-release/:/nfs-volume-release \
+-v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
 --workdir=/ \
 bosh/main-bosh-docker \
 /nfs-volume-release/scripts/run-bosh-release-tests-in-docker-env.sh
-
-# -e DEV=true \
--- scripts/run-driver-cert-locally.sh
+++ scripts/run-driver-cert-locally.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 docker run -it \
--v ${HOME}/workspace/nfs-volume-release/src:/go/src/ \
--v ${HOME}/workspace/nfs-volume-release:/nfs-volume-release \
--v ${HOME}/workspace/mapfs-release:/mapfs-release \
--v ${HOME}/workspace/docker_driver_integration_tests:/docker_driver_integration_tests \
+-v /Users/pivotal/workspace/nfs-volume-release/src:/go/src/ \
+-v /Users/pivotal/workspace/nfs-volume-release:/nfs-volume-release \
+-v /Users/pivotal/workspace/mapfs-release:/mapfs-release \
 -w / \
 -e TRANSPORT=tcp \
--e TEST_PACKAGE=docker_driver_integration_tests \
--e BINDINGS_FILE=nfs-bindings.json \
--e ERROR_CHECK_READONLY_MOUNTS=false \
--v ${HOME}/workspace/mapfs-release:/mapfs-release \
 --privileged \
 -u root \
 cfpersi/nfs-integration-tests \
