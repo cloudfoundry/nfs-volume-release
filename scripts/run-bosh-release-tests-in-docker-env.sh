86343d8b79d97bcd027df054484eac3c876b67cb
Diego Lemos <diego@diegolemos.net>
2023-10-04 11:57:27 +0100

    Revert "[ci] force Docker to use private registry"
    This reverts commit 8af27804b4303a31c92fd535d2826c458e9d91f8.

--- scripts/run-bosh-release-tests-in-docker-env.sh
+++ scripts/run-bosh-release-tests-in-docker-env.sh
@@ -15,7 +15,6 @@ source /tmp/local-bosh/director/env
 export DOCKER_TMP_DIR=$(find /tmp/ -name "tmp.*")
 
 docker \
---registry-mirror=https://harbor-repo.vmware.com/dockerhub-proxy-cache \
 --tls \
 --tlscacert=${DOCKER_TMP_DIR}/ca.pem \
 --tlscert=${DOCKER_TMP_DIR}/cert.pem \
