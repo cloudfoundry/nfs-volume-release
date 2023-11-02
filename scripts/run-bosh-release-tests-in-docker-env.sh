9bf16cc78b483f624b279a78ebc07c276e0eeed9
Diego Lemos <dlemos@vmware.com>
2023-11-02 10:24:15 +0000

    [ci] Make director use internal DNS server
    Signed-off-by: Konstantin Semenov <ksemenov@vmware.com>

--- scripts/run-bosh-release-tests-in-docker-env.sh
+++ scripts/run-bosh-release-tests-in-docker-env.sh
@@ -8,6 +8,14 @@ fi
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
+cp /usr/local/bosh-deployment/bosh.yml{,.original}
+
+bosh int \
+  /usr/local/bosh-deployment/bosh.yml.original \
+  -o /usr/local/bosh-deployment/misc/dns.yml \
+  -v internal_dns="[ $( cat /etc/resolv.conf | grep -im 1 '^nameserver' | cut -d ' ' -f2 ) ]" \
+  > /usr/local/bosh-deployment/bosh.yml
+
 export DOCKER_STORAGE_OPTIONS='--storage-opt dm.basesize=100G'
 . start-bosh
 
