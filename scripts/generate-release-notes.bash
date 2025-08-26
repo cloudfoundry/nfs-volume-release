ca6a6aa500c00e000b8f23a02df9aff0cda5c692
Amelia Downs <amelia.downs@broadcom.com>
2025-08-26 15:22:12 +0000

    improve generate-release-notes

--- scripts/generate-release-notes.bash
+++ scripts/generate-release-notes.bash
@@ -7,33 +7,35 @@ THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
 . "$CI/shared/helpers/release-note-helpers.bash"
 . "$CI/shared/helpers/git-helpers.bash"
-REPO_NAME=$(git_get_remote_name)
-REPO_PATH="${THIS_FILE_DIR}/../"
 unset THIS_FILE_DIR
 
-NFS_START_REF="${1}" # ex: "v0.0.7"
-NFS_END_REF="${2}" # ex: "v0.0.8"
+# ex. version_range="v0.343.0...v0.344.0"
+version_range="${1:?Please provide the start and end versions you want to generate release notes for './generate-release-notes.bash START_REF...END_REF' }"
+# ex. local_start_ref="v0.343.0"
+local_start_ref=$(get_start_ref_from_range "${version_range}")
+# ex. local_end_ref="v0.344.0"
+local_end_ref=$(get_end_ref_from_range "${version_range}")
 
-get_non_bot_commits "${NFS_START_REF}" "${NFS_END_REF}"
+get_non_bot_commits "${local_start_ref}" "${local_end_ref}"
 echo ""
 
-NFS_START_REF_DOCKER_DRIVER=$(git rev-parse "${NFS_START_REF}:src/code.cloudfoundry.org/dockerdriver")
-NFS_END_REF_DOCKER_DRIVER=$(git rev-parse "${NFS_END_REF}:src/code.cloudfoundry.org/dockerdriver")
+START_REF_DOCKER_DRIVER=$(git rev-parse "${local_start_ref}:src/code.cloudfoundry.org/dockerdriver")
+END_REF_DOCKER_DRIVER=$(git rev-parse "${local_end_ref}:src/code.cloudfoundry.org/dockerdriver")
 pushd src/code.cloudfoundry.org/dockerdriver > /dev/null
-  display_go_mod_diff "${NFS_START_REF_DOCKER_DRIVER}" "${NFS_END_REF_DOCKER_DRIVER}" go.mod "dockerdriver"
+  display_go_mod_diff "${START_REF_DOCKER_DRIVER}" "${END_REF_DOCKER_DRIVER}" go.mod "dockerdriver"
   echo ""
 popd > /dev/null
 
-display_go_mod_diff "${NFS_START_REF}" "${NFS_END_REF}" src/code.cloudfoundry.org/mapfs/go.mod "mapfs"
+display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/mapfs/go.mod "mapfs"
 echo ""
 
-display_go_mod_diff "${NFS_START_REF}" "${NFS_END_REF}" src/code.cloudfoundry.org/mapfs-performance-acceptance-tests/go.mod "mapfs-performance-acceptance-tests"
+display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/mapfs-performance-acceptance-tests/go.mod "mapfs-performance-acceptance-tests"
 echo ""
 
-display_go_mod_diff "${NFS_START_REF}" "${NFS_END_REF}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsbroker"
+display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsbroker"
 echo ""
 
-display_go_mod_diff "${NFS_START_REF}" "${NFS_END_REF}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsv3driver"
+display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsv3driver"
 echo ""
 
-display_blob_change_info "${NFS_START_REF}" "${NFS_END_REF}" config/blobs.yml
+display_blob_change_info "${local_start_ref}" "${local_end_ref}" config/blobs.yml
