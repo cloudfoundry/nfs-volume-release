e045d93832ee857cca66f0509fd84bcaa495ed08
Amelia Downs <amelia.downs@broadcom.com>
2025-11-07 21:10:46 +0000

    update generate-release-notes
    the helper has been updated to handle whitespace itself

--- scripts/generate-release-notes.bash
+++ scripts/generate-release-notes.bash
@@ -16,26 +16,16 @@ local_start_ref=$(get_start_ref_from_range "${version_range}")
 # ex. local_end_ref="v0.344.0"
 local_end_ref=$(get_end_ref_from_range "${version_range}")
 
-get_non_bot_commits "${local_start_ref}" "${local_end_ref}"
-echo ""
+display_non_bot_commits "${local_start_ref}" "${local_end_ref}"
 
 START_REF_DOCKER_DRIVER=$(git rev-parse "${local_start_ref}:src/code.cloudfoundry.org/dockerdriver")
 END_REF_DOCKER_DRIVER=$(git rev-parse "${local_end_ref}:src/code.cloudfoundry.org/dockerdriver")
 pushd src/code.cloudfoundry.org/dockerdriver > /dev/null
   display_go_mod_diff "${START_REF_DOCKER_DRIVER}" "${END_REF_DOCKER_DRIVER}" go.mod "dockerdriver"
-  echo ""
 popd > /dev/null
 
 display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/mapfs/go.mod "mapfs"
-echo ""
-
 display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/mapfs-performance-acceptance-tests/go.mod "mapfs-performance-acceptance-tests"
-echo ""
-
 display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsbroker"
-echo ""
-
 display_go_mod_diff "${local_start_ref}" "${local_end_ref}" src/code.cloudfoundry.org/nfsbroker/go.mod "nfsv3driver"
-echo ""
-
 display_blob_change_info "${local_start_ref}" "${local_end_ref}" config/blobs.yml
